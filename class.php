a8ba6a2ad2f7031e132a2fd706c80e591c7073a6
RestaurantController <46757266+RestaurantController@users.noreply.github.com>
2019-11-17 22:42:28 +0300

    Update class.php

--- class.php
+++ class.php
@@ -1,3 +1,40 @@
 <?php
-trigger_error("This plugin hasn't been set up yet", E_USER_ERROR);
+<?php
+class PHPQR {
+    public function generateQRWithArray($size, $raw){
+        if($raw == "" || $size == ""){
+            trigger_error("PHPQR Error: Cannot get data");
+        }
+        else {
+            $init = http_build_query(array(
+                "cht" => "qr",
+                "chs" => $size,
+                "chl" => $raw,
+                "choe" => "UTF-8"
+            ));
+            $url = "https://chart.googleapis.com/chart?".$init;
+            $returns = array(
+                "imgUrl" => $url,
+                "htmlCode" => "<img src='".$url."'>"
+            );
+            return $returns;
+        }
+    }
+    public function generateOnlyImageUrl($size, $raw){
+        if($raw == "" || $size == ""){
+            trigger_error("PHPQR Error: Cannot get data");
+        }
+        else {
+            $init = http_build_query(array(
+                "cht" => "qr",
+                "chs" => $size,
+                "chl" => $raw,
+                "choe" => "UTF-8"
+            ));
+            $url = "https://chart.googleapis.com/chart?".$init;
+            return $url;
+    }
+}
+}
+?>
 ?>
